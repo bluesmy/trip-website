857b6353d89900fcae12d3185d73a43c6a85ca19
Miao Yueh Su <bluesmy1020@gmail.com>
2020-04-22 16:32:41 +0800

    Create product model

---
+++ models/product.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.FLOAT,
+    type: DataTypes.STRING,
+    status: DataTypes.STRING
+  }, {});
+  Product.associate = function (models) {
+    // associations can be defined here
+  };
+  return Product;
+};
