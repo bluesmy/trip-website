d8fadab9f518b8abc9c4a9276fa0d707d7bd3a5c
Miao Yueh Su <bluesmy1020@gmail.com>
2020-04-22 10:32:15 +0800

    Add admin products index page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const productController = require('../controllers/productController.js')
+const adminController = require('../controllers/adminController.js')
 
 module.exports = app => {
 
   app.get('/', productController.getProducts)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/products'))
+  app.get('/admin/products', adminController.getProducts)
 
 }
