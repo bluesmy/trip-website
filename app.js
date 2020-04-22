f8439aab3f79245f6598fd9c471b244dcd3c6ec2
Miao Yueh Su <bluesmy1020@gmail.com>
2020-04-22 10:41:43 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const db = require('./models')
 
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
